bcf02174de75561e0fd92f7495462d64b072b46a
konnovK <konnov_k@protonmail.com>
2021-05-01 23:43:00 +0300

    Сделал возможность приближать и отдалять фрактал

--- src/fractals/Application.java
+++ src/fractals/Application.java
@@ -4,11 +4,14 @@ import fractals.fractal.Circle;
 import fractals.fractal.Fractal;
 import fractals.fractal.Mandelbrot;
 import fractals.palette.*;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -20,17 +23,21 @@ public class Application extends javafx.application.Application {
 
         Parent ui = initInterface();
         draw();
-        primaryStage.setScene(new Scene(ui, 800, 800));
+        primaryStage.setScene(new Scene(ui, 1280, 800));
 
-//        initActions();
+        initActions();
 
         primaryStage.show();
     }
 
     private final int width = 800;
     private final int height = 800;
+
     private final WritableImage image = new WritableImage(width, height);
     private final PixelWriter pixelWriter = image.getPixelWriter();
+
+    private final ImageView imageView = new ImageView(image);
+    private final Label coords = new Label();
 
     private final Fractal fractal = new Mandelbrot();
     private final Palette palette = new GrayPalette();
@@ -38,17 +45,47 @@ public class Application extends javafx.application.Application {
 
     private final boolean isIntPalette = true;
 
+    private double x0 = -2;
+    private double y0 = 2;
+    private double sizeOfView = 4;
+
     private Parent initInterface() {
         GridPane globalPane = new GridPane();
+        coords.setText("(" + (x0 + (sizeOfView / 2)) + ";" + (y0 - (sizeOfView / 2)) + "), size = " + sizeOfView);
 
-        ImageView imageView = new ImageView(image);
         globalPane.add(imageView, 0, 0);
+        globalPane.add(coords, 1, 0);
 
         return globalPane;
     }
 
+    private void initActions() {
+        EventHandler<MouseEvent> approximationHandler = (event) -> {
+            double _x, _y, _sizeOfView;
+
+            if (!event.isControlDown()) {
+                _x = event.getX();
+                _y = event.getY();
+                _sizeOfView = sizeOfView / 2;
+            } else {
+                _x = event.getX();
+                _y = event.getY();
+                _sizeOfView = sizeOfView * 2;
+            }
+
+            x0 = x0 + _x * (sizeOfView / width) - _sizeOfView / 2;
+            y0 = y0 - _y * (sizeOfView / width) + _sizeOfView / 2;
+            sizeOfView = _sizeOfView;
+
+            coords.setText("(" + (x0 + (sizeOfView / 2)) + ";" + (y0 - (sizeOfView / 2)) + "), size = " + sizeOfView);
+            drawFractal(x0, y0, sizeOfView);
+        };
+
+        imageView.onMouseClickedProperty().setValue(approximationHandler);
+    }
+
     private void draw() {
-        drawFractal(-2, 2, 4);
+        drawFractal(x0, y0, sizeOfView);
     }
 
     private void drawFractal(double x0, double y0, double sizeOfView) {
